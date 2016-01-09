c6e36f9bee0e40beab0643d0f98875fa56bca540
woliveiras <w.oliveira542@gmail.com>
2016-01-09 09:18:03 -0200

    Fix img folders

--- gulp.config.js
+++ gulp.config.js
@@ -1,10 +1,14 @@
-'use strict';
+module.exports = function () {
+  var config = {
+    dev               : './src/',
+    js                   : './src/js/',
+    mainjs        : 'main.js',
+    styl               : './src/styl/',
+    sprites       : './src/img/sprites/',
+    dist              : './dist/',
+    imgSrc       :'./src/img/',
+    imgDist     : './dist/img/'
+  };
 
-
-module.exports = {
-  lintFiles: [
-    './gulpfile.js',
-    './test/**/*.js',
-    'app/index.js'
-  ]
+  return config;
 };
