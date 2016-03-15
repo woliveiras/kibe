992961639139ef339539ee58d659ef9151e07cf5
darlanmendonca <darlanmendonca@gmail.com>
2016-03-15 11:43:01 -0300

    write module in es6, with babel transpiling

---
+++ test/unit/app.spec.es5.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var helpers = require('yeoman-test');
+var assert = require('yeoman-assert');
+var generator = process.env.PWD + '/app';
+
+describe('kibe application', scaffolding);
+
+function scaffolding() {
+  before(function (done) {
+    helpers.run(generator).on('end', done);
+  });
+
+  it('scaffolding', function () {
+    assert.file(['bower.json', '.editorconfig', '.gitignore', 'LICENSE.txt', 'bower.json', 'gulp.config.js', 'gulpfile.js', 'package.json', 'src/humans.txt', 'src/humans.txt', 'src/index.html', 'src/robots.txt', 'src/img', 'src/js', 'src/styl', './tasks/scripts.js', './tasks/styles.js', './tasks/imagemin.js', './tasks/sprites.js', './tasks/move.js', './tasks/browser-sync.js', './tasks/server.js', './tasks/default.js']);
+  });
+}
